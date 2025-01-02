28411fe13f9f46f76d3e67b0730048eff3d3482c
grahamd99 <graham.dodsworth@nhs.net>
2025-01-02 20:46:11 +0000

    main initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+
+// Configure Nunjucks templating engine
+nunjucks.configure(__dirname + '/views', {
+  autoescape: true,
+  express: app,
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true })); // Parse form data
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase');
+/*
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+*/
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define a schema and model
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+  res.render('index.njk', { title: 'Hello World Yeah!' });
+})
+
+// Handle form submission
+app.post('/submit', (req, res) => {
+  const userData = new User({
+    name: req.body.name,
+    email: req.body.email,
+  });
+
+  userData
+    .save()
+    .then(() => {
+      res.render('success.njk', { name: req.body.name });
+    })
+    .catch((err) => {
+      res.status(500).render('error.njk', { error: err });
+    });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
